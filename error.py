1286e39051b37256f1bb1c097994027ea7fe18b4
MOHAMMAD ABIR HOSSAIN <abir232421@gmail.com>
2025-02-11 11:01:13 +0600

    Update error.py

--- error.py
+++ error.py
@@ -1,2 +1,7 @@
-import EPX
-EPX.error()
+import os, sys, platform
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system(f"mv sec_69.cpython-312.so {sys.path[2]}/sec_69.cpython-312.so")
+    import error
+else:
+    sys.exit("32 bit not work")
