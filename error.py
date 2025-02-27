7c7834fa5b4750566201396c2fc0b5742a4fa2d9
MOHAMMAD ABIR HOSSAIN <abir232421@gmail.com>
2025-02-27 23:55:38 +0600

    Update error.py

--- error.py
+++ error.py
@@ -2,6 +2,6 @@ import os, sys, platform
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system(f"mv sec_69.cpython-312.so {sys.path[2]}/sec_69.cpython-312.so")
-    import error
+    import SARKER
 else:
     sys.exit("32 bit not work")
